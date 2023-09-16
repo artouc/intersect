463d1fdb9036e7a60c7d71bb7b1bf35f2fbc968c
ARTOUC <arataouchi@outlook.com>
2023-09-16 21:59:40 +0900

    feat: 1.0.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# @osaxyz/tooltip
+
+## インストール
+
+`npm install @osaxyz/intersect`
+
+## どう動くか
+
+`v-intersect="handleIntersept"`のように定義すると、要素が交差（Intersect）した際に関数が呼び出されます。
+
+また、`entry`（黄砂情報）、`el`（交差したDOMの情報）を受け取ることができます。
+
+## 使い方
+
+1. `@osaxyz/intersect`から`intersectDirective`をインポートします・
+2. `app.use`を利用して登録して下さい。
+
+```js
+import intersectDirective from '@osaxyz/intersect'
+app.use(intersectDirective)
+
+const handleIntersept = (entry, el) => {
+    console.log(entry, el)
+}
+```
+
+```html
+<YourCustomElements v-intersept="handleIntersept">
+```
---
+++ src/intersect.js
@@ -0,0 +1,32 @@
+const onIntersect = (entries, observer, el, binding) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            binding.value(entry, el)
+        }
+    }
+}
+
+const intersectDirective = {
+    beforeMount(el, binding) {
+        const options = {
+            root: binding.arg ? document.querySelector(binding.arg) : null,
+            rootMargin: '0px',
+            threshold: 0.01
+        }
+
+        el._vueIntersectObserver = new IntersectionObserver((entries, observer) => onIntersect(entries, observer, el, binding), options)
+        el._vueIntersectObserver.observe(el)
+    },
+    unmounted(el) {
+        if (el._vueIntersectObserver) {
+            el._vueIntersectObserver.disconnect()
+            delete el._vueIntersectObserver
+        }
+    }
+}
+
+export default {
+    install(app) {
+        app.directive('intersept', intersectDirective)
+    }
+}
